0e10064e738d07e7124477f85e8210cc81b65cb0
macrespo42 <maxime.cresopro@gmail.com>
2020-03-30 19:24:29 +0200

    main test ft_strlen

--- main.c
+++ main.c
@@ -6,14 +6,40 @@
 /*   By: macrespo <macrespo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/12 15:17:29 by macrespo          #+#    #+#             */
-/*   Updated: 2020/03/30 18:29:22 by macrespo         ###   ########.fr       */
+/*   Updated: 2020/03/30 19:24:08 by macrespo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libasm.h"
 
-int  main(void)
+static void strlen_test(const char *s)
 {
-	printf("strlen : mine [%lu] | original [%lu]", ft_strlen("hello"), strlen("hello"));
+    int     or;
+    int     ft;
+
+    or = strlen(s);
+    ft = ft_strlen(s);
+    printf("strlen : mine [%u] | original [%u]", ft, or);
+    if (or == ft)
+    {
+        printf("\033[1;32m");
+        printf(" [OK]\n");
+    }
+    else
+    {
+        printf("\033[1;31m");
+        printf(" [KO]\n");
+    }
+    printf("\033[0m");
+}
+
+int         main(void)
+{
+	strlen_test("Hello world");
+	strlen_test("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tristique dui at tellus blandit vulputate. In hac habitasse platea dictumst. In a nibh ");
+	strlen_test("");
+	strlen_test("\n");
+	strlen_test("\n\n");
+	strlen_test("111+}Te'st!wi/th*[<<As:c2ii3");
     return (0);
 }
