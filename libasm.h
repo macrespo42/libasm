1262b8622be590b9e412fb7fc9d031f2702a23b2
macrespo42 <maxime.cresopro@gmail.com>
2020-03-30 18:29:43 +0200

    add libasm header

--- libasm.h
+++ libasm.h
@@ -1,19 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   libasm.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: macrespo <macrespo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/03/12 15:17:29 by macrespo          #+#    #+#             */
-/*   Updated: 2020/03/30 18:29:22 by macrespo         ###   ########.fr       */
+/*   Created: 2020/03/30 17:31:34 by macrespo          #+#    #+#             */
+/*   Updated: 2020/03/30 18:28:03 by macrespo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libasm.h"
-
-int  main(void)
-{
-	printf("strlen : mine [%lu] | original [%lu]", ft_strlen("hello"), strlen("hello"));
-    return (0);
-}
+#ifndef LIBASM_H
+# define LIBASM_H
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+char    *ft_strdup(const char *s);
+char    *ft_strcpy(char *dest, const char *src);
+int     ft_strcmp(const char *s1, const char *s2);
+size_t  ft_strlen(const char *s);
+ssize_t ft_write(int fd, const void *buf, size_t count);
+ssize_t ft_read(int fd, void *buf, size_t count);
+#endif
