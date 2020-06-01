3b387f0a2bdc9a3c5faed08205b34311f620ea47
macrespo42 <maxime.crespopro@gmail.com>
2020-06-02 01:53:16 +0200

    test for mac

--- ft_write.s
+++ ft_write.s
@@ -17,21 +17,21 @@
 ;     call ___error
 ;     ret
 
-global  ft_write
-extern  ___error
-section .text
+            global  ft_write
+            extern  ___error
+            section .text
 
 ft_write:  push rbp
-        mov rbp, rsp
-        mov rax, 0x02000004
-        syscall
-        jc set_errno
-        jmp done
+            mov rbp, rsp
+            mov rax, 0x02000004
+            syscall
+            jc set_errno
+            jmp done
 
 set_errno:  mov r10, rax
-        call __error
-        mov [rax], r10
-        mov rax, -1
+            call ___error
+            mov [rax], r10
+            mov rax, -1
 
-done:   leave
-        ret
+done:       leave
+            ret
